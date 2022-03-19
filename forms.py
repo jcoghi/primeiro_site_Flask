cbd475a4fd3e5d3b7d40bf6aa3b1d8f8cd4d70d3
jcoghi <joao.fel@gmail.com>
2022-03-19 15:58:48 -0300

    Criação de mais uma página para edição dos posts

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField,FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from primeirositeflask.models import Usuario
 from flask_login import current_user
@@ -23,7 +23,6 @@ class FormCriarConta(FlaskForm):
         if usuario:
             raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
 
-
 class FormLogin(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
@@ -44,3 +43,9 @@ class FormEditarPerfil(FlaskForm):
             usuario = Usuario.query.filter_by(email=email.data).first()
             if usuario:
                 raise ValidationError('E-mail já cadastrado.')
+
+
+class FormCriarPost(FlaskForm):
+    titulo = StringField('Título do post', validators=[DataRequired(), Length(5, 140)])
+    corpo = TextAreaField('Escreva seu post aqui', validators=[DataRequired(), Length(50, 5000)])
+    botao_submit_criarpost = SubmitField('Ciar Post')
--- routes.py
+++ routes.py
@@ -1,35 +1,35 @@
 from flask import render_template, redirect, url_for, request, flash
 from primeirositeflask import app, database, bcrypt
-from primeirositeflask.forms import FormLogin, FormCriarConta, FormEditarPerfil
-from primeirositeflask.models import Usuario
+from primeirositeflask.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
+from primeirositeflask.models import Usuario, Post
 from flask_login import login_user, logout_user, current_user, login_required
 import secrets
 import os
 from PIL import Image
 
 
-lista_usuarios = ['Joao Felipe Fernandes Coghi', 'Gustavo Tozato']
-
 # Rota que direciona para o URL correto do site
 # Pode mudar a URL aqui a vontade. Assim o site não terá link quebrado
 
-
+# Página principal
 @app.route('/')
 def home():
-    return render_template('home.html')
+    posts = Post.query.order_by(Post.id.desc())
+    return render_template('home.html', posts=posts)
 
-
+# Página de contatos - Colocar link para GitHub, LinkedIn e uma breve descrição sobre os autores
 @app.route('/contato')
 def contatos():
     return render_template('contato.html')
 
-
+# Página com a lista de usuários -> Poderá ser excluído no futuro
 @app.route('/usuarios')
 @login_required
 def usuarios():
+    lista_usuarios = Usuario.query.all()
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
-
+# Para fazer o login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
@@ -38,7 +38,7 @@ def login():
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
         if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
             login_user(usuario, remember=form_login.lembrar_dados.data)
-            flash(f'Login realizado com sucesso no e-mail: {form_login.email.data}', 'alert-success')  # Mensagem sucesso
+            flash(f'Login realizado com sucesso no e-mail: {form_login.email.data}', 'alert-success')
             par_next = request.args.get('next')
             if par_next:
                 return redirect(par_next)
@@ -62,6 +62,7 @@ def login():
 
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
+# Para sair e desfazer o login
 @app.route('/sair')
 @login_required
 def sair():
@@ -69,13 +70,14 @@ def sair():
     flash(f'Logout realizado com sucesso', 'alert-success')  # Mensagem sucesso
     return redirect(url_for('home'))
 
+# Página de perfil
 @app.route('/perfil')
 @login_required
 def perfil():
     foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
     return render_template('perfil.html', foto_perfil=foto_perfil)
 
-
+# Lógica para salvar a imagem no banco de dados
 def salvar_imagem(imagem):
     codigo = secrets.token_hex(8)
     nome, extensao = os.path.splitext(imagem.filename)
@@ -87,7 +89,7 @@ def salvar_imagem(imagem):
     imagem_reduzida.save(caminho_completo)
     return nome_arquivo
 
-
+# Edição do perfil -> Precisa ter GET e POST para conversar com banco dados
 @app.route('/perfil/editar', methods=['GET', 'POST'])
 @login_required
 def editar_perfil():
@@ -107,8 +109,35 @@ def editar_perfil():
     foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
     return render_template('editar_perfil.html', foto_perfil=foto_perfil, form=form)
 
-@app.route('/post/criar')
+# Página de criação do Post
+@app.route('/post/criar', methods=['GET', 'POST'])
 @login_required
 def criar_post():
-    return render_template('criar_post.html')
+    form = FormCriarPost()
+    if form.validate_on_submit():
+        post = Post(titulo=form.titulo.data, corpo=form.corpo.data, autor=current_user)
+        database.session.add(post)
+        database.session.commit()
+        flash('Post criado com sucesso', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('criar_post.html', form=form)
 
+# Página do post
+@app.route('/post/<post_id>', methods=['GET', 'POST'])
+@login_required
+def exibir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        form = FormCriarPost()
+        if request.method == 'GET':
+            form.titulo.data = post.titulo
+            form.corpo.data = post.corpo
+        elif form.validate_on_submit():
+            post.titulo = form.titulo.data
+            post.corpo = form.corpo.data
+            database.session.commit()
+            flash('Post editado com sucesso', 'alert success')
+            return redirect(url_for('home'))
+    else:
+        form = None
+    return render_template('post.html', post=post, form=form)
